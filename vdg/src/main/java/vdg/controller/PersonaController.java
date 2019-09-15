d18d652131e1a4147ba3bd09b8287f7e79089c38
Gian Franco Grillo <gfgrillo3@gmail.com>
2019-09-15 15:43:00 -0300

    Agrego RequestMapping a agregarPersona

--- vdg/src/main/java/vdg/controller/PersonaController.java
+++ vdg/src/main/java/vdg/controller/PersonaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vdg.model.domain.Persona;
@@ -27,7 +28,8 @@ public class PersonaController {
 	}
 
 	@PostMapping
-	public Persona agregar(Persona persona) {
+	public Persona agregar(@RequestBody Persona persona) {
+		System.out.println(persona.getIdDireccion()+" ES EL ID DIREC\n"+persona.getIdUsuario()+" ES ID DE USER");
 		return personaRepo.save(persona);
 	}
 
