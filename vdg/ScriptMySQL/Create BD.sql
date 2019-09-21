b8d10802f67021b6331024d2d242167bcfa77936
Gian Franco Grillo <gfgrillo3@gmail.com>
2019-09-21 11:55:04 -0300

    Incidencias con los tipos y una de prueba

--- vdg/ScriptMySQL/Create BD.sql
+++ vdg/ScriptMySQL/Create BD.sql
@@ -8,8 +8,8 @@ DROP TABLE IF EXISTS Persona;
 DROP TABLE IF EXISTS Direccion;
 DROP TABLE IF EXISTS Usuario;
 DROP TABLE IF EXISTS RolDeUsuario;
-
-
+DROP TABLE IF EXISTS Incidencia;
+DROP TABLE IF EXISTS TipoIncidencia;
 
 
 CREATE TABLE RolDeUsuario (
@@ -76,6 +76,27 @@ CREATE TABLE RestriccionPerimetral (
 	FOREIGN KEY (idVictimario) REFERENCES Persona(idPersona)
 );
 
+CREATE TABLE Incidencia(
+	idIncidencia INT AUTO_INCREMENT,
+	fecha Timestamp,
+	descripcion VARCHAR(500),
+	topico VARCHAR (50),
+    idRestriccion int,
+	PRIMARY KEY (idIncidencia),
+	FOREIGN KEY (idRestriccion) REFERENCES RestriccionPerimetral(idRestriccion)
+);
+
+CREATE TABLE TipoIncidencia (
+	idTipoIncidencia INT AUTO_INCREMENT,
+	tipoIncidencia VARCHAR(50),
+	PRIMARY KEY (idTipoIncidencia)
+);
+
+INSERT INTO TipoIncidencia (tipoIncidencia) VALUES ('VictimarioIlocalizable');
+INSERT INTO TipoIncidencia (tipoIncidencia) VALUES ('DamnificadaIlocalizable');
+INSERT INTO TipoIncidencia (tipoIncidencia) VALUES ('PruebaDeVidaFallida');
+INSERT INTO TipoIncidencia (tipoIncidencia) VALUES ('InfraccionDeRestriccion');
+INSERT INTO TipoIncidencia (tipoIncidencia) VALUES ('FueraDeRutina');
 
 INSERT INTO RolDeUsuario (nombre) VALUES ('ADMINISTRATIVO');
 INSERT INTO RolDeUsuario (nombre) VALUES ('SUPERVISOR');
@@ -84,3 +105,4 @@ INSERT INTO RolDeUsuario (nombre) VALUES ('DAMNIFICADA');
 
 INSERT INTO Direccion (calle, altura, idLocalidad) VALUES ('Argerich', '1767', 2156);
 INSERT INTO Usuario (email, contrasena, rolDeUsuario) VALUES ("admin@admin.com", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", "SUPERVISOR");
+INSERT INTO vdg.Incidencia (fecha, descripcion, topico, idRestriccion) VALUES ("2019-09-20 20:42:35.677", "Esta es una incidencia de prueba", "FueraDeRutina", 1);
