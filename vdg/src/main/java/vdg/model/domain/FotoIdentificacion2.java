f3f5d3a0af816b8151e7f9f50692995200880eb3
gfgrillo3 <gfgrillo3@gmail.com>
2019-10-29 23:18:15 -0300

    Metodo para mostrar foto

---
+++ vdg/src/main/java/vdg/model/domain/FotoIdentificacion2.java
@@ -0,0 +1,44 @@
+package vdg.model.domain;
+
+import javax.persistence.*;
+
+
+public class FotoIdentificacion2 {
+	
+
+	private int idFoto;
+
+	private String foto;
+
+	
+	private int idPersona;
+
+	public FotoIdentificacion2() {
+		
+	}
+
+	public int getIdFoto() {
+		return idFoto;
+	}
+
+	public void setIdFoto(int idFoto) {
+		this.idFoto = idFoto;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public int getIdPersona() {
+		return idPersona;
+	}
+
+	public void setIdPersona(int idPersona) {
+		this.idPersona = idPersona;
+	}	
+	
+}
