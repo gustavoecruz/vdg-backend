f96d5ac68820518929398befa42c3caf0ae538d6
gfgrillo3 <gfgrillo3@gmail.com>
2019-11-02 21:30:01 -0300

    Fix falta requestBody

--- vdg/src/main/java/vdg/controller/FotoPruebaDeVidaController.java
+++ vdg/src/main/java/vdg/controller/FotoPruebaDeVidaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vdg.model.domain.FotoIdentificacion;
@@ -45,7 +46,7 @@ public class FotoPruebaDeVidaController {
 	}
 
 	@PostMapping("/{idPruebaDeVida}")
-	public FotoPruebaDeVida agregarFotoPruebaDeVida(@PathVariable("idPruebaDeVida") int idPruebaDeVida, String foto) {
+	public FotoPruebaDeVida agregarFotoPruebaDeVida(@PathVariable("idPruebaDeVida") int idPruebaDeVida, @RequestBody String foto) {
 		Decoder decoder = Base64.getDecoder();
 		byte[] decodedByte = decoder.decode(foto.split(",")[1]);
 		FotoPruebaDeVida fotoPruebaDeVida = new FotoPruebaDeVida();
